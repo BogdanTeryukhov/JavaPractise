e77f86895cbfc642d34560c40f702231e69c1855
BogdanTeryukhov <94743986+BogdanTeryukhov@users.noreply.github.com>
2022-02-19 15:03:25 +0300

    Add files via upload

---
+++ Лабы/Лаба2/Task2/Test.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Test {
+    public static void main (String[] args){
+        Vectors p = new Vectors(3.0, -2.0);
+        Vectors n = new Vectors(2.0, 8.0);
+        System.out.println(Vectors.getSkaMultiplication(p,n));
+    }
+}
+
+
